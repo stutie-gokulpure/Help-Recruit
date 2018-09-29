7fffde933c7d166e00be1ca0f0a4aeeff8692acb
stutie-gokulpure <43698126+stutie-gokulpure@users.noreply.github.com>
2018-09-29 15:09:01 +0530

    Add files via upload
    Initial Project Setup

---
+++ helpRecruit/REST/src/main/java/com/Circles/helpRecruit/HelpRecruitApplication.java
@@ -0,0 +1,12 @@
+package com.Circles.helpRecruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpRecruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpRecruitApplication.class, args);
+	}
+}
